79f3c903ea07418295c541f1ca12455621481f96
Thomas Rega <thomas.rega@safetyio.com>
2021-07-27 05:43:37 +0200

    Add basic admin functionality

--- m13/otto/admin.py
+++ m13/otto/admin.py
@@ -3,15 +3,28 @@ from .models import Address, Order, OrderItem
 
 
 class AddressAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        'last_name', 'first_name', 'street', 'house_number', 'zip_code')
 
 
 class OrderAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('marketplace_order_number', 'order_date')
 
 
 class OrderItemAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        'order_date',
+        'order_number',
+        'fulfillment_status',
+        'expected_delivery_date')
+
+    @admin.display()
+    def order_number(self, obj):
+        return obj.order.marketplace_order_number
+
+    @admin.display(ordering='order__order_date')
+    def order_date(self, obj):
+        return obj.order.order_date
 
 
 admin.site.register(Address, AddressAdmin)
