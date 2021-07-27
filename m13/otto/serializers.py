69f0fc91088435ffba3e5ac1f84c10d2c8dbb9c9
Thomas Rega <thomas.rega@safetyio.com>
2021-07-27 06:52:45 +0200

    WIP to get final CSV

--- m13/otto/serializers.py
+++ m13/otto/serializers.py
@@ -3,6 +3,38 @@ from rest_framework import serializers
 
 
 class OrderItemSerializer(serializers.HyperlinkedModelSerializer):
+    marketplace_order_number = serializers.CharField(
+        source='order.marketplace_order_number')
+    first_name = serializers.CharField(
+        source='order.delivery_address.first_name')
+    last_name = serializers.CharField(
+        source='order.delivery_address.last_name')
+    street = serializers.CharField(
+        source='order.delivery_address.street')
+    house_number = serializers.CharField(
+        source='order.delivery_address.house_number')
+    zip_code = serializers.CharField(
+        source='order.delivery_address.zip_code')
+    city = serializers.CharField(
+        source='order.delivery_address.city')
+    country_code = serializers.CharField(
+        source='order.delivery_address.country_code')
+
+    # Menge ?
+    # Positionstyp?
+
     class Meta:
         model = OrderItem
-        fields = ['ean', 'product_title']
+        fields = [
+            'marketplace_order_number',
+            'first_name',
+            'last_name',
+            'street',
+            'house_number',
+            'zip_code',
+            'city',
+            'country_code',
+            'sku',
+            'product_title',
+            'price_in_cent',
+        ]
