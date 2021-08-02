a84a6819a15d20932366752c250cb2cd4ac97287
Thomas Rega <thomas.rega@safetyio.com>
2021-08-02 07:33:03 +0200

    Add production config

---
+++ m13/m13/config/production.py
@@ -0,0 +1,22 @@
+import os
+
+from .base import *
+
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('PSQL_DB'),
+        'USER': os.getenv('PSQL_USER'),
+        'PASSWORD': os.getenv('PSQL_PASSWORD'),
+        'HOST': os.getenv('PSQL_HOST'),
+        'PORT': os.getenv('PSQL_PORT'),
+    }
+}
+
+DEBUG = False
+SECRET_KEY = os.getenv('M13_PRODUCTION_DJANGO_SECRET')
