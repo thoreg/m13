438496ee5fa13d9f034b1921f326d0b96680f0a8
Thomas Rega <thoreg@gmail.com>
2021-10-01 18:43:33 +0200

    Prepare order item csv download for Z

--- m13/zalando/urls.py
+++ m13/zalando/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('csv/<str:date>/', views.orderitems_csv, name='zalando_orderitems_csv'),
+    path('price-feed/', views.price_feed, name='zalando_price_feed'),
     path('oea/', views.oea_webhook, name='zalando_oea_webhook'),
     path('', views.index, name='zalando_index')
 ]
--- m13/zalando/views.py
+++ m13/zalando/views.py
@@ -1,8 +1,10 @@
+import csv
 import datetime as dt
 import json
 import logging
 from pprint import pformat, pprint
 from secrets import compare_digest
+from datetime import datetime, date, timedelta
 
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
@@ -23,17 +25,10 @@ LOG = logging.getLogger(__name__)
 
 
 @login_required
-def index(request):
+def price_feed(request):
     feed_uploads = FeedUpload.objects.all().order_by('-created')[:5]
-    order_items = (
-        OrderItem.objects.all()
-        .order_by('-order__order_date')
-        .select_related('order__delivery_address')[:100]
-    )
-    products = {p['ean']: p['title'] for p in Product.objects.all().values()}
 
     if request.method == 'POST':
-        LOG.info('form is valid')
         form = PriceToolForm(request.POST)
         if form.is_valid():
             update_z_factor(form.cleaned_data['z_factor'])
@@ -50,11 +45,27 @@ def index(request):
     ctx = {
         'feed_uploads': feed_uploads,
         'form': form,
-        'order_items': order_items,
-        'products': products,
         'z_factor': z_factor,
     }
+    return render(request, 'zalando/price_feed.html', ctx)
 
+
+@login_required
+def index(request):
+    order_items = (
+        OrderItem.objects.all()
+        .order_by('-order__order_date')
+        .select_related('order__delivery_address')[:100]
+    )
+    products = {p['ean']: p['title'] for p in Product.objects.all().values()}
+    ctx = {
+        'dbyesterday': (datetime.now() - timedelta(days=2)).strftime('%Y-%m-%d'),
+        'yesterday': (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d'),
+        'today': datetime.now().strftime('%Y-%m-%d'),
+        'order_items': order_items,
+        'products': products,
+    }
+    LOG.info(ctx)
     return render(request, 'zalando/index.html', ctx)
 
 
@@ -92,3 +103,142 @@ def oea_webhook(request):
 @atomic
 def process_oea_webhook_payload(payload):
     LOG.info(payload)
+
+
+@login_required
+def orderitems_csv(request, date):
+    """Return all processible orderitems as csv."""
+
+    LOG.info(request.__dict__)
+    LOG.info(f'GET: {request.GET.__dict__}')
+    LOG.info(f'date: {date}')
+
+    # Create the HttpResponse object with the appropriate CSV header.
+    response = HttpResponse(
+        content_type='text/csv',
+        headers={
+            'Content-Disposition': f'attachment; filename="{date}.csv"'},
+    )
+
+    response.write(u'\ufeff'.encode('utf8'))
+    writer = csv.writer(response, delimiter=';')
+    writer.writerow([
+        'Bestellnummer',
+        'Vorname',
+        'Name',
+        'Straße',
+        'PLZ',
+        'Ort',
+        'Land',
+        'Artikelnummer',
+        'Artikelname',
+        'Preis (Brutto)',
+        'Menge',
+        'Positionstyp',
+        'Anmerkung',
+        'EMAIL'
+    ])
+
+    # orderitems = (OrderItem.objects
+    #                        .filter(fulfillment_status='PROCESSABLE')
+    #                        .select_related('order__delivery_address')
+    #                        .order_by('order__marketplace_order_number'))
+    # print(f'Found {len(orderitems)} orderitems')
+
+    # current_order = None
+    # current_order_id = None
+    # for oi in orderitems:
+
+    #     # Append shipping information at the end of an order (after all orderitems)
+    #     if current_order_id and current_order_id != oi.order.marketplace_order_number:
+    #         price = '%0.2f' % current_order.delivery_fee[0]['deliveryFeeAmount']['amount']
+    #         price = price.replace('.', ',')
+    #         writer.writerow([
+    #             current_order.marketplace_order_number,
+    #             current_order.delivery_address.first_name,
+    #             current_order.delivery_address.last_name,
+    #             f'{current_order.delivery_address.street} {current_order.delivery_address.house_number}',
+    #             current_order.delivery_address.zip_code,
+    #             current_order.delivery_address.city,
+    #             _get_country_information(current_order.delivery_address.country_code),
+    #             ' ',
+    #             _get_delivery_info(current_order.delivery_fee[0]['name']),
+    #             price,
+    #             1,
+    #             'Versandposition',
+    #             f'OTTO {current_order.marketplace_order_id}',
+    #             'otto@manufaktur13.de'
+    #         ])
+
+    #     price = '%0.2f' % round(oi.price_in_cent / 100, 2)
+    #     price = price.replace('.', ',')
+
+    #     writer.writerow([
+    #         oi.order.marketplace_order_number,
+    #         oi.order.delivery_address.first_name,
+    #         oi.order.delivery_address.last_name,
+    #         f'{oi.order.delivery_address.street} {oi.order.delivery_address.house_number}',
+    #         oi.order.delivery_address.zip_code,
+    #         oi.order.delivery_address.city,
+    #         _get_country_information(oi.order.delivery_address.country_code),
+    #         oi.sku,
+    #         oi.product_title,
+    #         price,
+    #         1,
+    #         'Artikel',
+    #         f'OTTO {oi.order.marketplace_order_id}',
+    #         'otto@manufaktur13.de'
+    #     ])
+
+    #     current_order_id = oi.order.marketplace_order_number
+    #     current_order = deepcopy(oi.order)
+
+    # if current_order:
+    #     # extra locke for the last shipping position
+    #     price = '%0.2f' % current_order.delivery_fee[0]['deliveryFeeAmount']['amount']
+    #     price = price.replace('.', ',')
+    #     writer.writerow([
+    #         current_order.marketplace_order_number,
+    #         current_order.delivery_address.first_name,
+    #         current_order.delivery_address.last_name,
+    #         f'{current_order.delivery_address.street} {current_order.delivery_address.house_number}',
+    #         current_order.delivery_address.zip_code,
+    #         current_order.delivery_address.city,
+    #         _get_country_information(current_order.delivery_address.country_code),
+    #         ' ',
+    #         _get_delivery_info(current_order.delivery_fee[0]['name']),
+    #         price,
+    #         1,
+    #         'Versandposition',
+    #         f'OTTO {current_order.marketplace_order_id}',
+    #         'otto@manufaktur13.de'
+    #     ])
+
+    # email = request.GET.get('email')
+    # if email:
+    #     LOG.info('Houston we got to send an email')
+    #     if not settings.FROM_EMAIL_ADDRESS:
+    #         LOG.error('settings.FROM_EMAIL_ADDRESS needs to be defined')
+    #         return response
+
+    #     if not settings.OTTO_ORDER_CSV_RECEIVER_LIST:
+    #         LOG.error('settings.OTTO_ORDER_CSV_RECEIVER_LIST needs to be defined')
+    #         return response
+
+    #     LOG.info(f'settings.FROM_EMAIL_ADDRESS: {settings.FROM_EMAIL_ADDRESS}')
+    #     LOG.info(f'settings.OTTO_ORDER_CSV_RECEIVER_LIST: {settings.OTTO_ORDER_CSV_RECEIVER_LIST}')
+
+    #     message = EmailMessage(
+    #         f'OTTO Bestellungen - {now.strftime("%Y/%m/%d")}',
+    #         'OTTO Bestellungen als csv - Frohes Schaffen!!',
+    #         settings.FROM_EMAIL_ADDRESS,
+    #         settings.OTTO_ORDER_CSV_RECEIVER_LIST,
+    #     )
+    #     message.attach(
+    #         f'{now.strftime("%Y/%m/%d")}_otto_bestellungen.csv',
+    #         response.getvalue(),
+    #         'text/csv')
+    #     number_of_messages = message.send()
+    #     LOG.info(f'{number_of_messages} send')
+
+    return response
