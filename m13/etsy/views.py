696622be75d176b82a29370b88288bfd89b2b0e5
Thomas Rega <thoreg@gmail.com>
2021-10-29 06:58:33 +0200

    Extend logging

--- m13/etsy/views.py
+++ m13/etsy/views.py
@@ -35,8 +35,11 @@ def orders(request):
     refresh_token = 'NOT_SET_YET'
     try:
         auth_request = AuthRequest2.objects.all().order_by('-created')[0]
+
         LOG.info(auth_request.__dict__)
         LOG.info(auth_request.verifier, auth_request.created)
+        LOG.info('foobar')
+
         token = auth_request.auth_token
         refresh_token = auth_request.refresh_token
     except IndexError:
