055df425c05daf98587fac0662f580919c770fe6
Thomas Rega <thoreg@gmail.com>
2021-11-04 06:39:14 +0100

    [ etsy ] Add basic csv download for orderitems

--- m13/etsy/urls.py
+++ m13/etsy/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('csv', views.orderitems_csv, name='etsy_orderitems_csv'),
     path('orders', views.orders, name='etsy_orders'),
     path('oauth', views.oauth, name='etsy_oauth'),
     path('', views.index, name='etsy_index'),
--- m13/etsy/views.py
+++ m13/etsy/views.py
@@ -5,13 +5,16 @@ https://developers.etsy.com/documentation/essentials/authentication/#proof-key-f
 
 """
 import logging
+import csv
 import os
 from pprint import pformat
+from datetime import datetime
 
 import requests
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 from django.urls import reverse
+from django.http import HttpResponse
 
 from m13.common import base64_encode
 
@@ -34,6 +37,7 @@ def _render_auth_request_not_found(request):
     return render(request, 'etsy/oauth_error.html', ctx)
 
 
+@login_required
 def orders(request):
     """Display orders from etsy."""
     token = get_auth_token()
@@ -44,6 +48,124 @@ def orders(request):
     process_receipts(response)
 
     return redirect(reverse('etsy_index'))
+
+
+@login_required
+def orderitems_csv(request):
+    """Return all processible orderitems as csv."""
+    now = datetime.now()
+    now_as_str = now.strftime('%Y-%m-%dT%H_%M_%S')
+    # Create the HttpResponse object with the appropriate CSV header.
+    response = HttpResponse(
+        content_type='text/csv',
+        headers={
+            'Content-Disposition': f'attachment; filename="{now_as_str}.csv"'},
+    )
+
+    response.write(u'\ufeff'.encode('utf8'))
+    writer = csv.writer(response, delimiter=';')
+    writer.writerow([
+        'Bestellnummer',
+        'Vorname',
+        'Name',
+        'Straße',
+        'PLZ',
+        'Ort',
+        'Land',
+        'Artikelnummer',
+        'Artikelname',
+        'Preis (Brutto)',
+        'Menge',
+        'Positionstyp',
+        'Anmerkung',
+        'EMAIL'
+    ])
+
+    orderitems = (OrderItem.objects
+                           .filter(fulfillment_status='PAID')
+                           .select_related('order__delivery_address')
+                           .order_by('order__marketplace_order_id'))
+    print(f'Found {len(orderitems)} orderitems')
+
+    current_order = None
+    current_order_id = None
+    for oi in orderitems:
+
+        # Append shipping information at the end of an order (after all orderitems)
+        # if current_order_id and current_order_id != oi.order.marketplace_order_id:
+        #     price = '%0.2f' % current_order.delivery_fee[0]['deliveryFeeAmount']['amount']
+        #     price = price.replace('.', ',')
+        #     writer.writerow([
+        #         current_order.marketplace_order_id,
+        #         current_order.delivery_address.first_name,
+        #         current_order.delivery_address.last_name,
+        #         f'{current_order.delivery_address.street} {current_order.delivery_address.house_number}',
+        #         current_order.delivery_address.zip_code,
+        #         current_order.delivery_address.city,
+        #         _get_country_information(current_order.delivery_address.country_code),
+        #         ' ',
+        #         _get_delivery_info(current_order.delivery_fee[0]['name']),
+        #         price,
+        #         1,
+        #         'Versandposition',
+        #         f'OTTO {current_order.marketplace_order_id}',
+        #         'otto@manufaktur13.de'
+        #     ])
+
+        price = '%0.2f' % round(oi.price_in_cent / 100, 2)
+        price = price.replace('.', ',')
+
+        parsed_address = oi.order.delivery_address.formatted_address.split()
+
+        first_name = parsed_address[0]
+        last_name = parsed_address[1]
+        street = ' '.join(parsed_address[2:-3])
+        zip_code = parsed_address[-3]
+        city = parsed_address[-2]
+        country = parsed_address[-1]
+
+        writer.writerow([
+            oi.order.marketplace_order_id,
+            first_name,
+            last_name,
+            street,
+            zip_code,
+            city,
+            country,
+            oi.sku,
+            oi.product_title,
+            price,
+            1,
+            'Artikel',
+            f'{oi.order.marketplace_order_id}',
+            'etsy@manufaktur13.de'
+        ])
+
+        # current_order_id = oi.order.marketplace_order_id
+        # current_order = deepcopy(oi.order)
+
+    # if current_order:
+    #     # extra locke for the last shipping position
+    #     price = '%0.2f' % current_order.delivery_fee[0]['deliveryFeeAmount']['amount']
+    #     price = price.replace('.', ',')
+    #     writer.writerow([
+    #         current_order.marketplace_order_number,
+    #         current_order.delivery_address.first_name,
+    #         current_order.delivery_address.last_name,
+    #         f'{current_order.delivery_address.street} {current_order.delivery_address.house_number}',
+    #         current_order.delivery_address.zip_code,
+    #         current_order.delivery_address.city,
+    #         _get_country_information(current_order.delivery_address.country_code),
+    #         ' ',
+    #         _get_delivery_info(current_order.delivery_fee[0]['name']),
+    #         price,
+    #         1,
+    #         'Versandposition',
+    #         f'OTTO {current_order.marketplace_order_id}',
+    #         'otto@manufaktur13.de'
+    #     ])
+
+    return response
 
 
 def oauth(request):
