405c7ce70fc77a1cbc238a9b729178cb9018a095
Thomas Rega <thoreg@gmail.com>
2022-03-11 17:27:29 +0100

    [ otto ] Add stock sync

---
+++ m13/m13/lib/common.py
@@ -0,0 +1,7 @@
+"""Library for common tasks."""
+
+
+def chunk(lst, chunk_size):
+    """Walk through list in chunks."""
+    for i in range(0, len(lst), chunk_size):
+        yield lst[i:i + chunk_size]
