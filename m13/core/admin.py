738d83764732fe8f31f9f3303a6b08bd60a5f766
Thomas Rega <thoreg@gmail.com>
2022-05-31 06:44:26 +0200

    [ core ] Make product and article available via admin interface

--- m13/core/admin.py
+++ m13/core/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Article, Product
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'sku', 'ean')
+
+    @admin.display()
+    def product_name(self, obj):
+        return obj.product.name
+
+    @admin.display()
+    def product_ean(self, obj):
+        return obj.product.ean
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'ean')
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Product, ProductAdmin)
