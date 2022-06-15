62e470caf31d6eca51c9d4868d7a7e0843bdffe1
Thomas Rega <thoreg@gmail.com>
2022-06-15 11:15:20 +0200

    [ z ] Increase pagesize

--- m13/m13/config/base.py
+++ m13/m13/config/base.py
@@ -106,7 +106,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 100
 }
 
 DEFAULT_FROM_EMAIL = os.getenv('M13_FROM_EMAIL_ADDRESS')
