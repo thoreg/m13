64ee4f506e8f2afb1da742822e7b7263ed592013
Thomas Rega <thoreg@gmail.com>
2022-06-17 09:09:37 +0200

    [ z ] Send email if Zalando import crashes

--- m13/zalando/management/commands/m13_zalando_import_orders.py
+++ m13/zalando/management/commands/m13_zalando_import_orders.py
@@ -1,7 +1,7 @@
 import logging
-from functools import reduce
 
 from django.conf import settings
+from django.core.mail import send_mail
 from django.core.management.base import BaseCommand
 
 from zalando.services.orders import process_new_oea_records
@@ -14,4 +14,12 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         """..."""
-        process_new_oea_records()
+        try:
+            process_new_oea_records()
+        except Exception as exc:
+            send_mail(
+                'M13BM - Import of Zalando Orders failed',
+                f'{str(exc)}',
+                settings.FROM_EMAIL_ADDRESS,
+                settings.ADMINS,
+                fail_silently=False)
