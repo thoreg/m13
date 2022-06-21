3ec9d9699f4bfe8e873d612970b8cda18c6e0e94
Thomas Rega <thoreg@gmail.com>
2022-06-21 06:22:13 +0200

    [ z ] Add DatePicker to ZalandoCalculator Page

--- m13/m13-frontend/src/App.vue
+++ m13/m13-frontend/src/App.vue
@@ -1,14 +1,70 @@
 <template>
+<div>
+  <h1>{{ msg }}</h1>
+  <Datepicker v-model="dateRange" range :presetRanges="presetRanges" />
+  <p>
+    Selected Date Range: {{ dateRange }}
+  </p>
+  <p>
+    {{ getProductDataByDateRange }}
+  </p>
+
   <ZalandoCalculator msg="Zalando Calculator"/>
+</div>
 </template>
 
 <script>
 import ZalandoCalculator from './components/ZalandoCalculator.vue'
+import Datepicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css';
+
+import { ref } from 'vue';
+import { endOfMonth, endOfYear, startOfMonth, startOfYear, subMonths } from 'date-fns';
 
 export default {
   name: 'App',
   components: {
+    Datepicker,
     ZalandoCalculator
+  },
+  setup() {
+    const date = ref();
+
+    const presetRanges = ref([
+      {
+        label: 'This month',
+        range: [startOfMonth(new Date()), endOfMonth(new Date())]
+      },
+      {
+        label: 'Last month',
+        range: [startOfMonth(subMonths(new Date(), 1)), endOfMonth(subMonths(new Date(), 1))],
+      },
+      {
+        label: 'Last 3 months',
+        range: [startOfMonth(subMonths(new Date(), 3)), endOfMonth(new Date())],
+      },
+      {
+        label: 'This year',
+        range: [startOfYear(new Date()), endOfYear(new Date())]
+      },
+    ]);
+
+    return {
+      date,
+      presetRanges,
+    }
+  },
+  data() {
+    return {
+      dateRange: '',
+      msg: 'Zalando Calculator'
+    }
+  },
+  computed: {
+    getProductDataByDateRange() {
+      console.log(`date picked - from: ${this.dateRange[0]} to: ${this.dateRange[1]}`);
+      return 0;
+    }
   }
 }
 </script>
--- m13/m13-frontend/src/components/ZalandoCalculator.vue
+++ m13/m13-frontend/src/components/ZalandoCalculator.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>{{ msg }}</h1>
+  <div>
   <section class="overview">
     <div class="header" v-bind:style="{ 'background-color': statusColor(getAbsoluteDiff) }">
       _absolute_diff: {{ getAbsoluteDiff }}
@@ -61,49 +61,10 @@
       </div>
     </article>
   </section>
-  <hr style="margin: 5em 0;">
-  <h2>_original_table</h2>
-  <table>
-    <thead>
-      <th>Artikel</th>
-      <th>Kategorie</th>
-      <th>Herstellungskosten</th>
-      <th>VK (Zalando)</th>
-      <th>Retourenkosten</th>
-      <th>Versandkosten</th>
-      <th>8% Provision</th>
-      <th>19% MwSt.</th>
-      <th>Gemeinkosten</th>
-      <th>Gewinn nach Steuern</th>
-      <th>Stückzahl Verkauf</th>
-      <th>Stückzahl Retoure</th>
-      <th>Gewinn an Verkäufen</th>
-      <th>Verlust an Retouren</th>
-      <th>Differenz</th>
-    </thead>
-    <tr v-for="product in products" v-bind:key="product">
-      <td>{{ product.article }}</td>
-      <td>{{ product.category_name }}</td>
-      <td>{{ product.costs_production }}</td>
-      <td>{{ product.vk_zalando }}</td>
-      <td>{{ product.return_costs }}</td>
-      <td>{{ product.shipping_costs }}</td>
-      <td>{{ product.eight_percent_provision }}</td>
-      <td>{{ product.nineteen_percent_vat }}</td>
-      <td>{{ product.generic_costs }}</td>
-      <td>{{ product.profit_after_taxes }}</td>
-      <td>{{ product.shipped }}</td>
-      <td>{{ product.returned }}</td>
-      <td>{{ product.total_revenue }}</td>
-      <td>{{ product.total_return_costs }}</td>
-      <td v-bind:style="{ 'background-color': statusColor(product.total_diff) }">{{ product.total_diff }}</td>
-    </tr>
-  </table>
-
+</div>
 </template>
 
 <script>
-
 export default {
   name: 'ZalandoCalculator',
   props: {
@@ -118,6 +79,7 @@ export default {
         diff: 0
       },
       isOpen: true,
+      vModelExample: null,
     }
   },
   methods: {
