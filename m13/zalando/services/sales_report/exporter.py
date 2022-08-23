6518a64b11b9297e98f44329397b870b69eb8086
Thomas Rega <thoreg@gmail.com>
2022-08-23 10:03:14 +0200

    [ z ] Sort things properly

--- m13/zalando/services/sales_report/exporter.py
+++ m13/zalando/services/sales_report/exporter.py
@@ -35,7 +35,7 @@ def export_sales_report(from_order_date, to_order_date):
         SalesReport
         .objects
         .filter(shipping_return_date__range=(from_order_date, to_order_date))
-        .order_by('order_date')
+        .order_by('shipping_return_date__range')
     )
 
     for row in data:
