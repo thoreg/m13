436ba94b3850146cb25bbe39d90d9d953ba5de0e
Thomas Rega <thoreg@gmail.com>
2022-10-27 15:04:52 +0200

    [ gen ] Take rawshipmentreport items back to the dump

--- m13/scripts/dump_db.sh
+++ m13/scripts/dump_db.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 NOW=`date "+%Y%m%dT%H%M%S"`
-python manage.py dumpdata --natural-foreign --natural-primary -e contenttypes -e auth.Permission -e otto.StatsOrderItems -e zalando.StatsOrderItems -e etsy.StatsOrderItems -e zalando.rawdailyshipmentreport -e etsy -e admin --indent 2 > ${NOW}-dump.json
+python manage.py dumpdata      \
+    --natural-foreign          \
+    --natural-primary          \
+    -e contenttypes            \
+    -e auth.Permission         \
+    -e otto.StatsOrderItems    \
+    -e zalando.StatsOrderItems \
+    -e etsy.StatsOrderItems    \
+    -e etsy                    \
+    -e admin                   \
+    --indent 2 > ${NOW}-dump.json
 zip ${NOW}-dump.json.zip ${NOW}-dump.json
