574fcbc3492a83fe7ba20346f3e8e393dba566c6
Thomas Rega <thoreg@gmail.com>
2022-12-18 22:33:49 +0100

    [ generic ] Stop linter from crying

--- m13/m13/lib/common.py
+++ m13/m13/lib/common.py
@@ -4,4 +4,4 @@
 def chunk(lst, chunk_size):
     """Walk through list in chunks."""
     for i in range(0, len(lst), chunk_size):
-        yield lst[i : i + chunk_size]
+        yield lst[i: i + chunk_size]
