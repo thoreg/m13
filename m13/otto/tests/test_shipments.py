9029d3e1bb4247be1d05b9077319ed23cd7649d6
Thomas Rega <thoreg@gmail.com>
2023-02-20 10:27:21 +0100

    [ otto ] Make tests green again

--- m13/otto/tests/test_shipments.py
+++ m13/otto/tests/test_shipments.py
@@ -10,7 +10,7 @@ from otto.services.shipments import get_payload
 def test_get_payload():
     """Generate proper payload for shipment update endpoint out of given order."""
     order = Order.objects.get(marketplace_order_number="bzyrxkr8mz")
-    payload = get_payload(order, "123", "Hurz")
+    payload = get_payload(order, "123", "Hurz", "foo")
     expected = {
         "trackingKey": {"carrier": "Hurz", "trackingNumber": "123"},
         "shipDate": "2013-10-13T00:00:00Z",
@@ -19,7 +19,7 @@ def test_get_payload():
             {
                 "positionItemId": "36c49130-759d-4e88-92d2-d14af26250a4",
                 "salesOrderId": "239a3067-011f-4a5e-9a6c-7ddf4b2ff680",
-                "returnTrackingKey": {"carrier": "Hurz", "trackingNumber": "123"},
+                "returnTrackingKey": {"carrier": "Hurz", "trackingNumber": "foo"},
             }
         ],
     }
