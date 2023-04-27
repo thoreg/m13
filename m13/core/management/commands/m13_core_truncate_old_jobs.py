e209615b79ebdfb399bdfecf2c34bd6ab32c2ae2
Thomas Rega <thoreg@gmail.com>
2023-04-27 21:07:06 +0200

    [ core ] Cleanup old jobs which no one is interested in

---
+++ m13/core/management/commands/m13_core_truncate_old_jobs.py
@@ -0,0 +1,21 @@
+"""Remove all job results wich are older then X days."""
+from datetime import timedelta
+
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
+from core.models import Job
+from m13.lib.common import monitor
+
+MAX_AGAE_IN_HOURS = 48
+
+
+class Command(BaseCommand):
+    help = "Remove all job results wich are older then X days."
+
+    @monitor
+    def handle(self, *args, **kwargs):
+        """..."""
+        two_days_ago = timezone.now() - timedelta(hours=MAX_AGAE_IN_HOURS)
+        Job.objects.filter(created__lt=two_days_ago).delete()
+        return 0
