f6a34bf0c11ba587a828a6d9e8745fb23ab5e4c9
Thomas Rega <thoreg@gmail.com>
2024-02-04 07:12:23 +0100

    [ z ] Disable endpoint which is not used
    The data resulting from this is not used/accesses anyway

--- m13/zalando/urls.py
+++ m13/zalando/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("csv/<str:day>/", views.orderitems_csv, name="zalando_orderitems_csv"),
     path("price-feed/", views.price_feed, name="zalando_price_feed"),
-    path("oea/", views.oea_webhook, name="zalando_oea_webhook"),
+    # path("oea/", views.oea_webhook, name="zalando_oea_webhook"),
     path("stats/orderitems", views.stats_orderitems, name="zalando_stats_orderitems"),
     path("finance/upload", views.upload_files, name="zalando_finance_upload_files"),
     path(
