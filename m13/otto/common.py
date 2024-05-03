299524bcb30a4d1b8548eb64908ceab9f7ca1705
Thomas Rega <thoreg@gmail.com>
2024-05-03 06:11:02 +0200

    [ otto ] Use new auth tokens

--- m13/otto/common.py
+++ m13/otto/common.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+from pprint import pprint
 
 import requests
 
@@ -10,13 +11,20 @@ TOKEN_URL = "https://api.otto.market/v1/token"
 USERNAME = os.getenv("OTTO_API_USERNAME")
 PASSWORD = os.getenv("OTTO_API_PASSWORD")
 
+CLIENT_ID = os.getenv("OTTO_API_CLIENT_ID")
+CLIENT_SECRET = os.getenv("OTTO_API_CLIENT_SECRET")
+
 
 if not all([USERNAME, PASSWORD]):
     print("\nyou need to define username and password\n")
     sys.exit(1)
 
+if not all([CLIENT_ID, CLIENT_SECRET]):
+    print("\nyou need to define client_id and client_secret\n")
+    sys.exit(1)
 
-def get_auth_token():
+
+def __old__get_auth_token():
     """Get authentication token for further communication."""
     headers = {
         "Content-Type": "application/x-www-form-urlencoded",
@@ -32,5 +40,31 @@ def get_auth_token():
         TOKEN_URL,
         headers=headers,
         data=data,
+        timeout=60,
     )
+
+    pprint(r.json())
+    return r.json().get("access_token")
+
+
+def get_auth_token():
+    """Get authentication token for further communication."""
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "cache-control": "no-cache",
+    }
+    data = {
+        "grant_type": "client_credentials",
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+        "scope": "orders products",
+    }
+    r = requests.post(
+        TOKEN_URL,
+        headers=headers,
+        data=data,
+        timeout=60,
+    )
+
+    pprint(r.json())
     return r.json().get("access_token")
--- m13/otto/management/commands/m13_otto_check_product_prices.py
+++ m13/otto/management/commands/m13_otto_check_product_prices.py
@@ -1,31 +1,21 @@
 import logging
-import os
-import sys
 import time
+from pprint import pprint
 
 import requests
 from django.core.management.base import BaseCommand
 
 from core.models import Price
-from m13.lib.common import monitor
 from otto.common import get_auth_token
 
-PRODUCTS_URL = "https://api.otto.market/v2/products/"
-
-USERNAME = os.getenv("OTTO_API_USERNAME")
-PASSWORD = os.getenv("OTTO_API_PASSWORD")
+PRODUCTS_URL = "https://api.otto.market/v3/products/prices"
 
 LOG = logging.getLogger(__name__)
-
-if not all([USERNAME, PASSWORD]):
-    print("\nyou need to define username and password\n")
-    sys.exit(1)
 
 
 class Command(BaseCommand):
     help = "Get price information about product."
 
-    @monitor
     def handle(self, *args, **kwargs):
         """Read the price data of a single product variation."""
         token = get_auth_token()
@@ -33,18 +23,24 @@ class Command(BaseCommand):
             "Authorization": f"Bearer {token}",
         }
 
-        for price in Price.objects.all():
+        for price in Price.objects.all()[:200]:
             print(f"Fetching price for {price.sku} ...")
-            url = f"{PRODUCTS_URL}/{price.sku}/prices"
-            response = requests.get(url, headers=headers, timeout=60)
+            url = f"{PRODUCTS_URL}"
+            params = {"sku": price.sku}
+            response = requests.get(
+                url,
+                headers=headers,
+                timeout=60,
+                params=params,
+            )
 
             response_json = response.json()
-            print(response_json)
-            try:
-                price.vk_otto = response_json["standardPrice"]["amount"]
-                price.save()
-            except KeyError:
-                print(f"\n>>> No price found for: {price.sku}\n")
+            pprint(response_json)
+            # try:
+            #     price.vk_otto = response_json["standardPrice"]["amount"]
+            #     price.save()
+            # except KeyError:
+            #     print(f"\n>>> No price found for: {price.sku}\n")
 
             time.sleep(2)
 
