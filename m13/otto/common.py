0edee755fedee02ed082544e0b53431420366bfb
Thomas Rega <thoreg@gmail.com>
2025-04-09 16:34:16 +0200

    [ otto ] Extend scope of token request

--- m13/otto/common.py
+++ m13/otto/common.py
@@ -8,43 +8,13 @@ import requests
 
 TOKEN_URL = "https://api.otto.market/v1/token"
 
-USERNAME = os.getenv("OTTO_API_USERNAME")
-PASSWORD = os.getenv("OTTO_API_PASSWORD")
-
 CLIENT_ID = os.getenv("OTTO_API_CLIENT_ID")
 CLIENT_SECRET = os.getenv("OTTO_API_CLIENT_SECRET")
 
 
-if not all([USERNAME, PASSWORD]):
-    print("\nyou need to define username and password\n")
-    sys.exit(1)
-
 if not all([CLIENT_ID, CLIENT_SECRET]):
     print("\nyou need to define client_id and client_secret\n")
     sys.exit(1)
-
-
-def __old__get_auth_token():
-    """Get authentication token for further communication."""
-    headers = {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "cache-control": "no-cache",
-    }
-    data = {
-        "username": USERNAME,
-        "grant_type": "password",
-        "client_id": "token-otto-api",
-        "password": PASSWORD,
-    }
-    r = requests.post(
-        TOKEN_URL,
-        headers=headers,
-        data=data,
-        timeout=60,
-    )
-
-    pprint(r.json())
-    return r.json().get("access_token")
 
 
 def get_auth_token():
@@ -57,7 +27,7 @@ def get_auth_token():
         "grant_type": "client_credentials",
         "client_id": CLIENT_ID,
         "client_secret": CLIENT_SECRET,
-        "scope": "orders products shipments",
+        "scope": "orders products shipments availability",
     }
     r = requests.post(
         TOKEN_URL,
