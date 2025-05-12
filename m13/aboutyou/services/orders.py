12d67449bd3089e998b068f599f3f01106414ee1
Thomas Rega <thoreg@gmail.com>
2025-05-12 11:16:07 +0200

    [ ay ] Go next

--- m13/aboutyou/services/orders.py
+++ m13/aboutyou/services/orders.py
@@ -20,79 +20,90 @@ LOG = logging.getLogger(__name__)
 def sync():
     LOG.info("sync_orders starting ....")
 
-    # Setup headers
-    token = os.getenv("M13_ABOUTYOU_TOKEN")
-    if not token:
-        LOG.error("M13_ABOUTYOU_TOKEN not found")
-        return
-    headers = {
-        "X-API-Key": token,
-    }
-    response = requests.get(
-        f"{ORDERS_URL}",
-        headers=headers,
-        timeout=60,
-    )
-    if response.status_code != requests.codes.ok:
-        LOG.error("fetching orders failed")
-        LOG.error(response.json())
-        return
-
-    for entry in response.json()["items"]:
-
-        marketplace_order_id = entry.get("order_number")
-        status = entry.get("status")
-
-        delivery_address, _created = Address.objects.get_or_create(
-            addition=entry.get("shipping_additional"),
-            city=entry.get("shipping_city"),
-            country_code=entry.get("shipping_country_code"),
-            first_name=entry.get("shipping_recipient_first_name"),
-            last_name=entry.get("shipping_recipient_last_name"),
-            street=entry.get("shipping_street"),
-            zip_code=entry.get("shipping_zip_code"),
+    def _import_orders(url: str) -> dict:
+        token = os.getenv("M13_ABOUTYOU_TOKEN")
+        if not token:
+            LOG.error("M13_ABOUTYOU_TOKEN not found")
+            return
+        headers = {
+            "X-API-Key": token,
+        }
+        response = requests.get(
+            url,
+            headers=headers,
+            timeout=60,
         )
-        invoice_address, _created = Address.objects.get_or_create(
-            addition=entry.get("billing_additional"),
-            city=entry.get("billing_city"),
-            country_code=entry.get("billing_country_code"),
-            first_name=entry.get("billing_recipient_first_name"),
-            last_name=entry.get("billing_recipient_last_name"),
-            street=entry.get("billing_street"),
-            zip_code=entry.get("billing_zip_code"),
-        )
+        if response.status_code != requests.codes.ok:
+            LOG.error("fetching orders failed")
+            LOG.error(response.json())
+            return
 
-        order, created = Order.objects.get_or_create(
-            marketplace_order_id=marketplace_order_id,
-            defaults={
-                "delivery_address": delivery_address,
-                "invoice_address": invoice_address,
-                "order_date": entry.get("created_at"),
-                "status": Order.Status(status),
-            },
-        )
-        if created:
-            LOG.info(f"Order {marketplace_order_id} imported")
-        else:
-            LOG.info(f"Order {marketplace_order_id} already known")
-            order.status = Order.Status(status)
-            order.save()
+        for entry in response.json()["items"]:
 
-        for oi in entry.get("order_items"):
-            fulfillment_status = oi["status"]
-            position_item_id = oi["id"]
-            sku = oi["sku"]
+            marketplace_order_id = entry.get("order_number")
+            status = entry.get("status")
 
-            order_item, created = OrderItem.objects.get_or_create(
-                order=order,
-                position_item_id=position_item_id,
+            delivery_address, _created = Address.objects.get_or_create(
+                addition=entry.get("shipping_additional"),
+                city=entry.get("shipping_city"),
+                country_code=entry.get("shipping_country_code"),
+                first_name=entry.get("shipping_recipient_first_name"),
+                last_name=entry.get("shipping_recipient_last_name"),
+                street=entry.get("shipping_street"),
+                zip_code=entry.get("shipping_zip_code"),
+            )
+            invoice_address, _created = Address.objects.get_or_create(
+                addition=entry.get("billing_additional"),
+                city=entry.get("billing_city"),
+                country_code=entry.get("billing_country_code"),
+                first_name=entry.get("billing_recipient_first_name"),
+                last_name=entry.get("billing_recipient_last_name"),
+                street=entry.get("billing_street"),
+                zip_code=entry.get("billing_zip_code"),
+            )
+
+            order, created = Order.objects.get_or_create(
+                marketplace_order_id=marketplace_order_id,
                 defaults={
-                    "fulfillment_status": fulfillment_status,
-                    "price_in_cent": oi["price_with_tax"],
-                    "sku": sku,
-                    "vat_rate": oi["vat"],
+                    "delivery_address": delivery_address,
+                    "invoice_address": invoice_address,
+                    "order_date": entry.get("created_at"),
+                    "status": Order.Status(status),
                 },
             )
-            if not created:
-                order_item.fulfillment_status = fulfillment_status
-                order_item.save()
+            if created:
+                LOG.info(f"Order {marketplace_order_id} imported")
+            else:
+                LOG.info(f"Order {marketplace_order_id} already known")
+                order.status = Order.Status(status)
+                order.save()
+
+            for oi in entry.get("order_items"):
+                fulfillment_status = oi["status"]
+                position_item_id = oi["id"]
+                sku = oi["sku"]
+
+                order_item, created = OrderItem.objects.get_or_create(
+                    order=order,
+                    position_item_id=position_item_id,
+                    defaults={
+                        "fulfillment_status": fulfillment_status,
+                        "price_in_cent": oi["price_with_tax"],
+                        "sku": sku,
+                        "vat_rate": oi["vat"],
+                    },
+                )
+                if not created:
+                    order_item.fulfillment_status = fulfillment_status
+                    order_item.save()
+
+        return response.json()
+
+    resp = _import_orders(ORDERS_URL)
+
+    next_url = resp["pagination"]["next"]
+    if next_url:
+        LOG.info(f"Next page: {next_url}")
+        _import_orders(f"{API_BASE_URL}{next_url}")
+
+    LOG.info("sync_orders finished ...")
